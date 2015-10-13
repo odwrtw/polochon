3f5f3b6107117ac9dcfc6eb1bef28096434384ab
Nicolas Duhamel <nicolas@jombi.fr>
2015-10-13 18:13:09 +0200

    Use build constraint to fix #41

--- server/polochon.go
+++ server/polochon.go
@@ -3,22 +3,6 @@ package main
 import (
 	"flag"
 	"log"
-
-	// Modules
-	_ "github.com/odwrtw/polochon/modules/addicted"
-	_ "github.com/odwrtw/polochon/modules/canape"
-	_ "github.com/odwrtw/polochon/modules/eztv"
-	_ "github.com/odwrtw/polochon/modules/fsnotify"
-	_ "github.com/odwrtw/polochon/modules/imdb"
-	_ "github.com/odwrtw/polochon/modules/inotify"
-	_ "github.com/odwrtw/polochon/modules/openguessit"
-	_ "github.com/odwrtw/polochon/modules/opensubtitles"
-	_ "github.com/odwrtw/polochon/modules/pushover"
-	_ "github.com/odwrtw/polochon/modules/tmdb"
-	_ "github.com/odwrtw/polochon/modules/transmission"
-	_ "github.com/odwrtw/polochon/modules/tvdb"
-	_ "github.com/odwrtw/polochon/modules/yifysubs"
-	_ "github.com/odwrtw/polochon/modules/yts"
 )
 
 func main() {
