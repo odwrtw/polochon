45c80c499a0382e21dbed3718070bcdfbc5c360f
Julien Rolland <julien@rolland.io>
2015-11-22 20:03:29 +0000

    Fix -X main.minversion warning

--- builds/cross.sh
+++ builds/cross.sh
@@ -20,7 +20,7 @@ echo '
 
     echo "Building for $os $arch$armv" >&2
     binname="polochon_${os}_${arch}${armv}"
-    time go build -v -a -o "$binname" -ldflags "-X main.minversion $VERSION" server/*.go || {
+    time go build -v -a -o "$binname" -ldflags "-X main.minversion=$VERSION" server/*.go || {
       echo "Unable to build for $os $arch$armv" >&2
       continue
     }
