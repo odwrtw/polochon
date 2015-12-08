b5c31b5353ea35526e6e3728c23b2d995c066625
Julien Rolland <julien@rolland.io>
2015-12-08 23:27:10 +0000

    Parallelize builds

--- builds/cross.sh
+++ builds/cross.sh
@@ -8,23 +8,30 @@ echo '
   while read os arch armv; do
     [ -n "$os" ] || continue
 
-    export VERSION=`git rev-parse --short HEAD`
-    export GIMME_OS="$os"
-    export GIMME_TYPE="source"
-    export GOOS="$os"
-    export GIMME_ARCH="$arch"
-    export GOARCH="$arch"
-    export GOARM="$armv"
-    eval "$(gimme 1.5)"
+    {
+        export VERSION=`git rev-parse --short HEAD`
+        export GIMME_OS="$os"
+        export GIMME_TYPE="source"
+        export GOOS="$os"
+        export GIMME_ARCH="$arch"
+        export GOARCH="$arch"
+        export GOARM="$armv"
+        eval "$(gimme 1.5)"
 
-    echo "Building for $os $arch$armv" >&2
-    binname="polochon_${os}_${arch}${armv}"
-    time go build -v -a -o "$binname" -ldflags "-X main.minversion=$VERSION" server/*.go || {
-      echo "Unable to build for $os $arch$armv" >&2
-      continue
-    }
-    file $binname
+        echo "Building for $os $arch$armv" >&2
+        binname="polochon_${os}_${arch}${armv}"
+        time go build -a -v -o "$binname" -ldflags "-X main.minversion=$VERSION" server/*.go || {
+          echo "Unable to build for $os $arch$armv" >&2
+          exit 1
+        }
+        file $binname
+    } &
 
     [ -z "`git tag -l | grep latest`" ] && break
   done
 }
+
+ls -1
+
+wait
+exit 0
