848b5516439c47b793b9a9afd78e04c9491e4612
Julien Rolland <julien@rolland.io>
2015-12-15 20:41:37 +0000

    Speeding up the build
    * Use a Makefile instead of bash script
    * Use github.com/aktau/github-release to prevent gem install failures
    * Build only on master branch

---
+++ scripts/travis_github_releases.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+# FIXME: /home/travis/build.sh: line 53: assert: unbound variable
+# set -o nounset
+
+readonly SCRIPT_NAME=$(basename $0)
+readonly TAG_DATE=$(date -u "+%Y-%m-%d %H:%M:%S UTC")
+readonly GIT_TAG=latest
+readonly GITHUB_PROJECT=https://${GITHUB_TOKEN}@github.com/odwrtw/polochon
+readonly RELEASE_DESC="Generated from TravisCI build ${TRAVIS_BUILD_NUMBER} (${TAG_DATE})"
+
+log() {
+    echo -e "\e[36m-->\e[39m \e[35m$@\e[39m"
+    logger -p user.notice -t $SCRIPT_NAME "$@"
+}
+
+tag() {
+    log "Calling tag()"
+
+    git config --global user.email "builds@travis-ci.com"
+    git config --global user.name "Travis CI"
+
+    git fetch ${GITHUB_PROJECT} --tags
+
+    if [ -n "$(git tag -l ${GIT_TAG})" ]
+    then
+        log "Deleting existing tag ${GIT_TAG}..."
+        git tag -d ${GIT_TAG}
+        git push ${GITHUB_PROJECT} :refs/tags/${GIT_TAG}
+    fi
+
+    log "Creating tag ${GIT_TAG}..."
+    git tag ${GIT_TAG} -a -m "${RELEASE_DESC}"
+    git push ${GITHUB_PROJECT} ${GIT_TAG}
+}
+
+release() {
+    log "Calling release()"
+
+    log "Installing github-release..."
+    go get github.com/aktau/github-release
+
+    if [ -n "$(github-release info -t ${GIT_TAG} >/dev/null 2>&1)" ]
+    then
+        log "Deleting existing release with tag ${GIT_TAG}..."
+        github-release delete --tag ${GIT_TAG}
+    fi
+
+    log "Creating fresh release ${GIT_TAG}..."
+    github-release release \
+        --tag  ${GIT_TAG} \
+        --name ${GIT_TAG} \
+        --description "${RELEASE_DESC}"
+
+    cd builds
+    for BIN_NAME in $(ls polochon_*)
+    do
+        log "Pushing ${BIN_NAME} to release ${GIT_TAG}..."
+        github-release upload \
+            --tag  ${GIT_TAG} \
+            --name ${BIN_NAME} \
+            --file ${BIN_NAME}
+    done
+}
+
+if [ ${TRAVIS_BRANCH} == "master" ] && [ ${TRAVIS_PULL_REQUEST} == "false" ]
+then
+    tag
+    release
+else
+    log "No need to create the ${GIT_TAG} tag on this branch"
+fi
