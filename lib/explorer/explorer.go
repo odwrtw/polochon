18d5f5aa525ea1593fa6c164d5ee76e91f8b1d7a
Grégoire Delattre <gregoire.delattre@gmail.com>
2015-12-27 17:52:26 +0100

    Add the first "explorer" implementation

---
+++ lib/explorer/explorer.go
@@ -0,0 +1,22 @@
+package explorer
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/odwrtw/polochon/lib"
+)
+
+// Option represents the query type of explorer
+type Option int
+
+// Available explorer options
+const (
+	BySeeds Option = iota
+)
+
+// Explorer is the interface explore new videos from different sources
+type Explorer interface {
+	AvailableMovieOptions() []Option
+	GetMovieList(option Option, log *logrus.Entry) ([]*polochon.Movie, error)
+	AvailableShowOptions() []Option
+	GetShowList(option Option, log *logrus.Entry) ([]*polochon.Show, error)
+}
--- modules/kickass/kickass.go
+++ modules/kickass/kickass.go
@@ -6,15 +6,23 @@ import (
 
 	"gopkg.in/yaml.v2"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/odwrtw/guessit"
 	"github.com/odwrtw/kickass"
 	"github.com/odwrtw/polochon/lib"
+	"github.com/odwrtw/polochon/lib/explorer"
 )
 
 // Module constants
 const (
 	moduleName = "kickass"
+)
+
+// Category represents the category of a movie or a tv show
+type Category string
+
+// Availabe categories
+const (
+	MoviesCategory Category = "movies"
+	ShowsCategory  Category = "tv"
 )
 
 // Custom errors
@@ -25,15 +33,6 @@ var (
 // Register yts as a Torrenter
 func init() {
 	polochon.RegisterTorrenter(moduleName, NewFromRawYaml)
-}
-
-// Searcher is an interface to search for torrents
-type Searcher interface {
-	validate() error
-	searchStr() string
-	category() string
-	users() []string
-	isValidGuess(*guessit.Response, *logrus.Entry) bool
 }
 
 // Params represents the module params
@@ -56,11 +55,19 @@ func NewFromRawYaml(p []byte) (polochon.Torrenter, error) {
 		return nil, err
 	}
 
-	return New(params)
+	return NewTorrenter(params)
 }
 
-// New returns a new kickass
-func New(params *Params) (polochon.Torrenter, error) {
+// NewTorrenter returns a new kickass torrenter
+func NewTorrenter(params *Params) (polochon.Torrenter, error) {
+	return &Kickass{
+		client: kickass.New(),
+		Params: params,
+	}, nil
+}
+
+// NewExplorer returns a new explorer
+func NewExplorer(params *Params) (explorer.Explorer, error) {
 	return &Kickass{
 		client: kickass.New(),
 		Params: params,
@@ -72,108 +79,8 @@ func (k *Kickass) Name() string {
 	return moduleName
 }
 
-// GetTorrents implements the torrenter interface
-func (k *Kickass) GetTorrents(i interface{}, log *logrus.Entry) error {
-	switch video := i.(type) {
-	case *polochon.Movie:
-		m := NewMovieSearcher(video, k.MoviesUsers)
-		t, err := k.search(m, log)
-		if err != nil {
-			return err
-		}
-		m.Torrents = t
-		return nil
-	case *polochon.ShowEpisode:
-		s := NewShowEpisodeSearcher(video, k.ShowsUsers)
-		t, err := k.search(s, log)
-		if err != nil {
-			return err
-		}
-		s.Torrents = t
-		return nil
-	default:
-		return ErrInvalidType
-	}
-}
-
-func (k *Kickass) search(s Searcher, log *logrus.Entry) ([]polochon.Torrent, error) {
-	users := s.users()
-	result := []polochon.Torrent{}
-
-	log = log.WithFields(logrus.Fields{
-		"search_category": s.category(),
-		"search_string":   s.searchStr(),
-	})
-
-	if err := s.validate(); err != nil {
-		log.Error(err)
-		return nil, err
-	}
-
-	for _, u := range users {
-		torrents, err := k.searchUser(s, log, u)
-		if err != nil {
-			return nil, err
-		}
-
-		result = append(result, torrents...)
-	}
-
-	return polochon.FilterTorrents(result), nil
-}
-
-func (k *Kickass) searchUser(s Searcher, log *logrus.Entry, user string) ([]polochon.Torrent, error) {
-	query := &kickass.Query{
-		User:     user,
-		OrderBy:  "seeders",
-		Order:    "desc",
-		Category: s.category(),
-		Search:   s.searchStr(),
-	}
-	log = log.WithField("search_user", user)
-
-	torrents, err := k.client.Search(query)
-	if err != nil {
-		return nil, err
-	}
-
-	result := []polochon.Torrent{}
-	for _, t := range torrents {
-		// Hack to make the torrent name look like a video name so that guessit
-		// can guess the title, year and quality
-		torrentStr := strings.Replace(t.Name, " ", ".", -1) + ".mp4"
-		guess, err := guessit.Guess(torrentStr)
-		if err != nil {
-			continue
-		}
-
-		if !s.isValidGuess(guess, log) {
-			continue
-		}
-
-		// Default quality
-		if s.category() == "tv" && guess.Quality == "" {
-			guess.Quality = string(polochon.Quality480p)
-		}
-
-		// Get the torrent quality
-		torrentQuality := polochon.Quality(guess.Quality)
-		if !torrentQuality.IsAllowed() {
-			log.Infof("kickass: unhandled quality: %q", torrentQuality)
-			continue
-		}
-
-		log.WithFields(logrus.Fields{
-			"torrent_quality": guess.Quality,
-			"torrent_name":    torrentStr,
-		}).Debug("Adding torrent to the list")
-
-		// Add the torrent
-		result = append(result, polochon.Torrent{
-			Quality: torrentQuality,
-			URL:     t.MagnetURL,
-		})
-	}
-
-	return result, nil
+func torrentGuessitStr(t *kickass.Torrent) string {
+	// Hack to make the torrent name look like a video name so that guessit
+	// can guess the title, year and quality
+	return strings.Replace(t.Name, " ", ".", -1) + ".mp4"
 }
