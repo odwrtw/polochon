33ddfca753874116f9fb130d63d235cf6b89ed6a
Julien Rolland <julien@rolland.io>
2016-01-12 15:56:59 +0000

    Add version flag

--- app/polochon.go
+++ app/polochon.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/odwrtw/polochon/app/internal/app"
 )
 
+var (
+	// VersionString contains the latest described commit from build
+	VersionString string
+	// RevisionString contains the latest commit
+	RevisionString string
+)
+
 func main() {
 	configPath := flag.String("configPath", "../config.yml", "path of the configuration file")
 	tokenPath := flag.String("tokenPath", "", "path of the token file")
+	versionFlag := flag.Bool("version", false, "show version number and quit")
+
 	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("polochon %s\nLatest commit: %s\n", VersionString, RevisionString[0:6])
+		os.Exit(0)
+	}
 
 	app, err := app.NewApp(*configPath, *tokenPath)
 	if err != nil {
