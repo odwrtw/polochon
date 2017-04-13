29bdc231f8b83f034274b7440af8d527bd1ce2ff
Lucas BEE <pouulet@gmail.com>
2017-04-13 13:58:42 +0200

    Add the tmdb movie Searcher

---
+++ modules/tmdb/searcher.go
@@ -0,0 +1,56 @@
+package tmdb
+
+import (
+	"github.com/Sirupsen/logrus"
+	polochon "github.com/odwrtw/polochon/lib"
+)
+
+// Register tmdb as a Searcher
+func init() {
+	polochon.RegisterSearcher(moduleName, NewSearcher)
+}
+
+// NewSearcher creates a new Tmdb Searcher
+func NewSearcher(p []byte) (polochon.Searcher, error) {
+	return NewFromRawYaml(p)
+}
+
+// SearchMovie implements the polochon Searcher interface
+func (t *TmDB) SearchMovie(key string, log *logrus.Entry) ([]*polochon.Movie, error) {
+	// Add year option if given
+	options := map[string]string{
+		"include_adult": "false",
+	}
+
+	// Search on tmdb
+	r, err := tmdbSearchMovie(t.client, key, options)
+	if err != nil {
+		log.Debugf("error while trying to find movie with %q", key)
+		return nil, err
+	}
+	// Check if there is any results
+	if len(r.Results) == 0 {
+		log.Debugf("failed to find movie with %q", key)
+		return nil, ErrNoMovieFound
+	}
+
+	result := []*polochon.Movie{}
+	for _, tMovie := range r.Results {
+		m := polochon.NewMovie(polochon.MovieConfig{})
+		m.TmdbID = tMovie.ID
+		err = t.getMovieDetails(m)
+		if err != nil {
+			log.Warnf("error while getting tmdb movie details %q", err)
+			continue
+		}
+		result = append(result, m)
+	}
+
+	return result, nil
+}
+
+// SearchShow implements the polochon Searcher interface
+// Not implemented
+func (t *TmDB) SearchShow(key string, log *logrus.Entry) ([]*polochon.Show, error) {
+	return nil, ErrInvalidArgument
+}
