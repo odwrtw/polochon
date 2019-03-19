b9d65f820439a0744daf8cb18de68069c853c587
Lucas BEE <pouulet@gmail.com>
2019-03-19 16:53:37 +0100

    Searcher is now a Module

--- lib/searcher.go
+++ lib/searcher.go
@@ -8,6 +8,7 @@ import (
 
 // Searcher is the interface to search shows or movies from different sources
 type Searcher interface {
+	Module
 	SearchMovie(key string, log *logrus.Entry) ([]*Movie, error)
 	SearchShow(key string, log *logrus.Entry) ([]*Show, error)
 }
