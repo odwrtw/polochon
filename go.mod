3617fe9f742f1b7db0446c34e151095865466402
Grégoire Delattre <gregoire.delattre@gmail.com>
2019-04-18 15:28:59 +0200

    Update the lib used to query the tvdb api

--- go.mod
+++ go.mod
@@ -7,15 +7,11 @@ require (
 	github.com/arbovm/levenshtein v0.0.0-20160628152529-48b4e1c0c4d0
 	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
 	github.com/fsnotify/fsnotify v1.4.7 // indirect
-	github.com/garfunkel/go-tvdb v0.0.0-20150227144245-59b4220a6dad
 	github.com/gorilla/mux v1.7.1
-	github.com/gorilla/rpc v1.2.0 // indirect
 	github.com/gregdel/argo v0.0.0-20190104143955-4ac365771987
 	github.com/gregdel/pushover v0.0.0-20190217183207-15d3fef40636
 	github.com/jpillora/scraper v0.0.0-20171020112404-0b5e5ce320ff // indirect
 	github.com/kolo/xmlrpc v0.0.0-20190408202923-bec0502dc24d // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/kylelemons/go-gypsy v0.0.0-20160905020020-08cad365cd28 // indirect
 	github.com/meatballhat/negroni-logrus v0.0.0-20170801195057-31067281800f
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
@@ -34,13 +30,11 @@ require (
 	github.com/odwrtw/yts v0.0.0-20190414102655-a00dd999bdf4
 	github.com/oz/osdb v0.0.0-20190204162748-da06ada9cdc1
 	github.com/phyber/negroni-gzip v0.0.0-20180113114010-ef6356a5d029
+	github.com/pioz/tvdb v0.0.0-20190107104821-ffdf1011c717
 	github.com/ryanbradynd05/go-tmdb v0.0.0-20181220020137-291a20d25ffd
 	github.com/sirupsen/logrus v1.4.1
-	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/urfave/negroni v1.0.0
-	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	golang.org/x/net v0.0.0-20190415214537-1da14a5a36f2 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7
 	gopkg.in/unrolled/render.v1 v1.0.0
 	gopkg.in/xmlpath.v2 v2.0.0-20150820204837-860cbeca3ebc // indirect
--- modules/tvdb/tvdb.go
+++ modules/tvdb/tvdb.go
@@ -3,13 +3,15 @@ package tvdb
 import (
 	"errors"
 	"fmt"
+	"net/url"
+	"sort"
 	"strconv"
 	"time"
 
-	"github.com/arbovm/levenshtein"
-	"github.com/garfunkel/go-tvdb"
-	"github.com/odwrtw/polochon/lib"
+	polochon "github.com/odwrtw/polochon/lib"
+	"github.com/pioz/tvdb"
 	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
 )
 
 // Register tvdb as a Detailer
@@ -18,106 +20,197 @@ func init() {
 	polochon.RegisterCalendar(moduleName, NewCalendarFromRawYaml)
 }
 
-// API constants
-const (
-	APIendpoint = "http://www.thetvdb.com/api"
-	Token       = "1D62F2F90030C444"
-	AssetsURL   = "http://thetvdb.com/banners/"
-)
-
 // Module constants
 const (
-	moduleName = "tvdb"
+	moduleName      = "tvdb"
+	tokenExpiration = 24 * time.Hour
 )
 
 // Errors
 var (
 	ErrShowNotFound                   = errors.New("tvdb: show not found")
-	ErrSeasonNotFound                 = errors.New("tvdb: season not found")
-	ErrEpisodeNotFound                = errors.New("tvdb: episode not found")
-	ErrShowUpdate                     = errors.New("tvdb: failed to get details for show")
-	ErrMissingShowTitle               = errors.New("tvdb: missing show title")
-	ErrMissingShowImdbID              = errors.New("tvdb: missing show imdb id")
+	ErrShowImageNotFound              = errors.New("tvdb: show image not found")
 	ErrNotEnoughArguments             = errors.New("tvdb: not enough arguments to perform search")
 	ErrInvalidArgument                = errors.New("tvdb: invalid argument type")
 	ErrMissingShowEpisodeInformations = errors.New("tvdb: missing show episode informations to get details")
 	ErrFailedToUpdateEpisode          = errors.New("tvdb: failed to update episode details")
 )
 
+// Params represents the module params
+type Params struct {
+	ApiKey   string `yaml:"api_key"`
+	UserID   string `yaml:"user_id"`
+	Username string `yaml:"username"`
+}
+
 // TvDB implents the Detailer interface
-type TvDB struct{}
+type TvDB struct {
+	client           *tvdb.Client
+	lastTokenRefresh *time.Time
+}
 
-// NewDetailerFromRawYaml unmarshals the bytes as yaml as params and call the
-// New function
+// NewFromRawYaml creates a new TvDB from YML config
+func NewFromRawYaml(p []byte) (*TvDB, error) {
+	params := &Params{}
+	if err := yaml.Unmarshal(p, params); err != nil {
+		return nil, err
+	}
+
+	return New(params)
+}
+
+// New returns a new tvdb module
+func New(params *Params) (*TvDB, error) {
+	return &TvDB{
+		client: &tvdb.Client{
+			Apikey:   params.ApiKey,
+			Username: params.Username,
+			Userkey:  params.UserID,
+		},
+	}, nil
+}
+
+// NewDetailerFromRawYaml implements the detailer interface
 func NewDetailerFromRawYaml(p []byte) (polochon.Detailer, error) {
-	return NewDetailer()
+	return NewFromRawYaml(p)
 }
 
-// NewDetailer returns an initialized tmdb instance as a detailer
-func NewDetailer() (polochon.Detailer, error) {
-	return &TvDB{}, nil
-}
-
-// NewCalendarFromRawYaml unmarshals the bytes as yaml as params and call the
-// New function
+// NewCalendarFromRawYaml implements the calendar interface
 func NewCalendarFromRawYaml(p []byte) (polochon.Calendar, error) {
-	return NewCalendar()
+	return NewFromRawYaml(p)
 }
 
-// NewCalendar returns an initialized tmdb instance as a calendar
-func NewCalendar() (polochon.Calendar, error) {
-	return &TvDB{}, nil
+// Name implements the Module interface
+func (t *TvDB) Name() string {
+	return moduleName
 }
 
-// assetURL return the URL of an asset
-func assetURL(asset string) string {
-	return AssetsURL + asset
+// login handles the token refresh
+func (t *TvDB) login() error {
+	if t.lastTokenRefresh != nil && time.Since(*t.lastTokenRefresh) < tokenExpiration/2 {
+		// The token is still valid
+		return nil
+	}
+
+	// Never logged in or the token has expired
+	var f func() error
+	if t.lastTokenRefresh == nil || time.Since(*t.lastTokenRefresh) > tokenExpiration {
+		f = t.client.Login
+	} else {
+		f = t.client.RefreshToken
+	}
+
+	if err := f(); err != nil {
+		t.lastTokenRefresh = nil
+		return err
+	}
+
+	now := time.Now()
+	t.lastTokenRefresh = &now
+	return nil
 }
 
-// Get show detail
-var getShowFromTvdb = func(s *tvdb.Series) error {
-	return s.GetDetail()
+// Status implements the Module interface
+func (t *TvDB) Status() (polochon.ModuleStatus, error) {
+	// Search for The Matrix on trakttv via imdbID
+	_, err := t.searchByImdbID("tt2085059")
+	if err != nil {
+		return polochon.StatusFail, err
+	}
+
+	return polochon.StatusOK, nil
 }
 
-// updateShow update the show information from the tvdb infos
-func updateShow(s *polochon.Show, tvSeries *tvdb.Series) error {
-	err := getShowFromTvdb(tvSeries)
+func (t *TvDB) searchByImdbID(id string) (*tvdb.Series, error) {
+	if err := t.login(); err != nil {
+		return nil, err
+	}
+
+	series, err := t.client.SearchByImdbID(id)
+	if err != nil {
+		if tvdb.HaveCodeError(404, err) {
+			return nil, ErrShowNotFound
+		}
+
+		return nil, err
+	}
+
+	if len(series) == 0 {
+		return nil, ErrShowNotFound
+	}
+
+	return &series[0], nil
+}
+
+func (t *TvDB) searchByName(query string) (*tvdb.Series, error) {
+	if err := t.login(); err != nil {
+		return nil, err
+	}
+
+	series, err := t.client.BestSearch(query)
+	if err != nil {
+		if tvdb.HaveCodeError(404, err) {
+			return nil, ErrShowNotFound
+		}
+
+		return nil, err
+	}
+
+	return &series, nil
+}
+
+// GetDetails implements the Detailer interface
+func (t *TvDB) GetDetails(i interface{}, log *logrus.Entry) error {
+	switch v := i.(type) {
+	case *polochon.Show:
+		return t.getShowDetails(v, nil)
+	case *polochon.ShowEpisode:
+		return t.getEpisodeDetails(v)
+	default:
+		return ErrInvalidArgument
+	}
+}
+
+func (t *TvDB) searchShow(s *polochon.Show) (*tvdb.Series, error) {
+	var show *tvdb.Series
+	var err error
+
+	switch {
+	case s.ImdbID != "":
+		show, err = t.searchByImdbID(s.ImdbID)
+	case s.Title != "":
+		// Add the year to the search if defined
+		query := s.Title
+		if s.Year != 0 {
+			query = fmt.Sprintf("%s (%d)", query, s.Year)
+		}
+
+		show, err = t.searchByName(query)
+	default:
+		return nil, ErrNotEnoughArguments
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if show == nil {
+		return nil, ErrShowNotFound
+	}
+
+	return show, nil
+}
+
+func (t *TvDB) getShowEpisodes(s *polochon.Show, show *tvdb.Series, params url.Values) error {
+	err := t.client.GetSeriesEpisodes(show, params)
 	if err != nil {
 		return err
 	}
 
-	s.Title = tvSeries.SeriesName
-	s.Plot = tvSeries.Overview
-	s.TvdbID = int(tvSeries.ID)
-	s.URL = fmt.Sprintf("%s/%s/series/%d/all/en.zip", APIendpoint, Token, s.TvdbID)
-	s.ImdbID = tvSeries.ImdbID
-	s.Banner = assetURL(tvSeries.Banner)
-	s.Fanart = assetURL(tvSeries.Fanart)
-	s.Poster = assetURL(tvSeries.Poster)
-
-	// Get the year from the first aired date
-	if tvSeries.FirstAired != "" {
-		date, err := time.Parse("2006-01-02", tvSeries.FirstAired)
-		if err != nil {
-			return err
-		}
-		s.Year = date.Year()
-		s.FirstAired = &date
-	}
-
-	// Convert rating string to a float
-	if tvSeries.Rating != "" {
-		f, err := strconv.ParseFloat(tvSeries.Rating, 32)
-		if err != nil {
-			return err
-		}
-		s.Rating = float32(f)
-	}
-
 	// Get runtime
 	var runtime int
-	if tvSeries.Runtime != "" {
-		r, err := strconv.Atoi(tvSeries.Runtime)
+	if show.Runtime != "" {
+		r, err := strconv.Atoi(show.Runtime)
 		if err != nil {
 			return err
 		}
@@ -126,113 +219,124 @@ func updateShow(s *polochon.Show, tvSeries *tvdb.Series) error {
 
 	// Go through each episode from the list
 	s.Episodes = []*polochon.ShowEpisode{}
-	for _, episodeList := range tvSeries.Seasons {
-		for _, e := range episodeList {
-			episode := polochon.NewShowEpisode(polochon.ShowConfig{})
-			episode.Title = e.EpisodeName
-			episode.ShowTitle = s.Title
-			episode.Season = int(e.SeasonNumber)
-			episode.Episode = int(e.EpisodeNumber)
-			episode.TvdbID = int(e.ID)
-			episode.Aired = e.FirstAired
-			episode.Plot = e.Overview
-			episode.Runtime = runtime
-			episode.Thumb = assetURL(e.Filename)
-			episode.ShowImdbID = s.ImdbID
-			episode.ShowTvdbID = s.TvdbID
-			episode.EpisodeImdbID = e.ImdbID
+	for _, e := range show.Episodes {
+		episode := polochon.NewShowEpisode(polochon.ShowConfig{})
+		episode.Title = e.EpisodeName
+		episode.ShowTitle = s.Title
+		episode.Season = e.AiredSeason
+		episode.Episode = e.AiredEpisodeNumber
+		episode.TvdbID = e.ID
+		episode.Aired = e.FirstAired
+		episode.Plot = e.Overview
+		episode.Runtime = runtime
+		episode.Thumb = tvdb.ImageURL(e.Filename)
+		episode.ShowImdbID = s.ImdbID
+		episode.ShowTvdbID = s.TvdbID
+		episode.EpisodeImdbID = e.ImdbID
+		episode.Rating = e.SiteRating
 
-			if e.Rating != "" {
-				f, err := strconv.ParseFloat(e.Rating, 32)
-				if err != nil {
-					return err
-				}
-				episode.Rating = float32(f)
-			}
-
-			// Add the episode to the list
-			s.Episodes = append(s.Episodes, episode)
-		}
+		// Add the episode to the list
+		s.Episodes = append(s.Episodes, episode)
 	}
 
 	return nil
 }
 
-// Function to be overwritten during the tests
-var tvdbGetSeries = func(name string) (seriesList tvdb.SeriesList, err error) {
-	return tvdb.GetSeries(name)
-}
+func (t *TvDB) getShowImages(s *polochon.Show, show *tvdb.Series) error {
+	// Update the banner
+	s.Banner = show.BannerURL()
 
-// getShowByName helps find a show on tvdb using its name
-func getShowByName(s *polochon.Show) error {
-	if s.Title == "" {
-		return ErrMissingShowTitle
-	}
-
-	// Add the year to the search if defined
-	query := s.Title
-	if s.Year != 0 {
-		query = fmt.Sprintf("%s (%d)", query, s.Year)
-	}
-
-	// Search on tvdb by name
-	list, err := tvdbGetSeries(query)
-	if err != nil {
-		return err
-	}
-
-	// Any result ?
-	if len(list.Series) == 0 {
-		return ErrShowNotFound
-	}
-
-	// Find the most accurate serie base on the levenshtein distance
-	var show *tvdb.Series
-	minDistance := 100
-	for _, tvdbSerie := range list.Series {
-		d := levenshtein.Distance(query, tvdbSerie.SeriesName)
-		if d < minDistance {
-			minDistance = d
-			show = tvdbSerie
+	// Update the poster and fannart
+	for _, imageType := range []struct {
+		f   func(s *tvdb.Series) error
+		url *string
+		t   string
+	}{
+		{
+			t:   "fanart",
+			url: &s.Fanart,
+			f:   t.client.GetSeriesFanartImages,
+		},
+		{
+			t:   "poster",
+			url: &s.Poster,
+			f:   t.client.GetSeriesPosterImages,
+		},
+	} {
+		// Fetch the image
+		if err := imageType.f(show); err != nil {
+			return err
 		}
+
+		images := []*tvdb.Image{}
+		for _, i := range show.Images {
+			if i.KeyType != imageType.t {
+				continue
+			}
+			images = append(images, &i)
+		}
+
+		if images == nil || len(images) == 0 {
+			return ErrShowImageNotFound
+		}
+
+		// Sort images by ratings and count
+		sort.Slice(images, func(i, j int) bool {
+			avgI := images[i].RatingsInfo.Average
+			avgJ := images[j].RatingsInfo.Average
+			if avgI == avgJ {
+				countI := images[i].RatingsInfo.Count
+				countJ := images[j].RatingsInfo.Count
+				return countI > countJ
+			}
+			return avgI > avgJ
+		})
+
+		// Update the image url
+		*imageType.url = tvdb.ImageURL(images[0].FileName)
 	}
 
-	return updateShow(s, show)
+	return nil
 }
 
-// Function to be overwritten during the tests
-var tvdbGetShowByImdbID = func(id string) (series *tvdb.Series, err error) {
-	return tvdb.GetSeriesByIMDBID(id)
-}
-
-// get show by imdb id
-func getShowByImdbID(s *polochon.Show) error {
-	if s.ImdbID == "" {
-		return ErrMissingShowImdbID
-	}
-
-	show, err := tvdbGetShowByImdbID(s.ImdbID)
+func (t *TvDB) getShowDetails(s *polochon.Show, params url.Values) error {
+	// Search for the show
+	show, err := t.searchShow(s)
 	if err != nil {
 		return err
 	}
 
-	return updateShow(s, show)
-}
-
-// getShowDetails
-func getShowDetails(s *polochon.Show) error {
-	switch {
-	case s.ImdbID != "":
-		return getShowByImdbID(s)
-	case s.Title != "":
-		return getShowByName(s)
-	default:
-		return ErrNotEnoughArguments
+	// Get the show details
+	err = t.client.GetSeries(show)
+	if err != nil {
+		return err
 	}
+
+	s.Title = show.SeriesName
+	s.Plot = show.Overview
+	s.TvdbID = show.ID
+	s.ImdbID = show.ImdbID
+	s.Rating = show.SiteRating
+
+	// Get the year from the first aired date
+	if show.FirstAired != "" {
+		date, err := time.Parse("2006-01-02", show.FirstAired)
+		if err != nil {
+			return err
+		}
+		s.Year = date.Year()
+		s.FirstAired = &date
+	}
+
+	// Get show images
+	if err := t.getShowImages(s, show); err != nil {
+		return err
+	}
+
+	return t.getShowEpisodes(s, show, params)
 }
 
-// getShowEpisodeDetails
-func getShowEpisodeDetails(s *polochon.ShowEpisode) error {
+func (t *TvDB) getEpisodeDetails(s *polochon.ShowEpisode) error {
 	// The season / episode infos are needed
 	if s.Season == 0 || s.Episode == 0 {
 		return ErrMissingShowEpisodeInformations
@@ -258,7 +362,12 @@ func getShowEpisodeDetails(s *polochon.ShowEpisode) error {
 		show.ImdbID = s.ShowImdbID
 	}
 
-	err := getShowDetails(show)
+	params := url.Values{
+		"airedSeason":  {strconv.Itoa(s.Season)},
+		"airedEpisode": {strconv.Itoa(s.Episode)},
+	}
+
+	err := t.getShowDetails(show, params)
 	if err != nil {
 		return err
 	}
@@ -292,69 +401,33 @@ func getShowEpisodeDetails(s *polochon.ShowEpisode) error {
 	return nil
 }
 
-// Name implements the Module interface
-func (t *TvDB) Name() string {
-	return moduleName
-}
-
-// Status implements the Module interface
-func (t *TvDB) Status() (polochon.ModuleStatus, error) {
-	// Search for The Matrix on trakttv via imdbID
-	_, err := tvdbGetShowByImdbID("tt2085059")
-	if err != nil {
-		return polochon.StatusFail, err
-	}
-
-	return polochon.StatusOK, nil
-}
-
-// GetDetails implements the Detailer interface
-func (t *TvDB) GetDetails(i interface{}, log *logrus.Entry) error {
-	switch v := i.(type) {
-	case *polochon.Show:
-		return getShowDetails(v)
-	case *polochon.ShowEpisode:
-		return getShowEpisodeDetails(v)
-	default:
-		return ErrInvalidArgument
-	}
-}
-
 // GetShowCalendar implements the Calendar interface
 func (t *TvDB) GetShowCalendar(show *polochon.Show, log *logrus.Entry) (*polochon.ShowCalendar, error) {
-	// Get show details
-	if show.ImdbID == "" {
-		return nil, ErrMissingShowImdbID
+	if err := t.getShowDetails(show, nil); err != nil {
+		return nil, err
+	}
+
+	if show == nil || show.ImdbID == "" {
+		return nil, ErrShowNotFound
 	}
 	calendar := polochon.NewShowCalendar(show.ImdbID)
 
-	s, err := tvdb.GetSeriesByIMDBID(show.ImdbID)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := s.GetDetail(); err != nil {
-		return nil, err
-	}
-
-	for s, es := range s.Seasons {
-		for _, e := range es {
-			episodeCalendar := &polochon.ShowCalendarEpisode{
-				Season:  int(s),
-				Episode: int(e.EpisodeNumber),
+	// Get show details
+	for _, e := range show.Episodes {
+		var aired time.Time
+		var err error
+		if e.Aired != "" {
+			aired, err = time.Parse("2006-01-02", e.Aired)
+			if err != nil {
+				return nil, err
 			}
-
-			// Parse aired date
-			if e.FirstAired != "" {
-				aired, err := time.Parse("2006-01-02", e.FirstAired)
-				if err != nil {
-					return nil, err
-				}
-				episodeCalendar.AiredDate = &aired
-			}
-
-			calendar.Episodes = append(calendar.Episodes, episodeCalendar)
 		}
+
+		calendar.Episodes = append(calendar.Episodes, &polochon.ShowCalendarEpisode{
+			Season:    e.Season,
+			Episode:   e.Episode,
+			AiredDate: &aired,
+		})
 	}
 
 	return calendar, nil
