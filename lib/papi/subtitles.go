1baab51dacb9c9ffcbee176c8854b069fa513248
Grégoire Delattre <gregoire.delattre@gmail.com>
2026-03-25 00:17:42 +0100

    Add subtitles list and download in papi

--- lib/papi/subtitles.go
+++ lib/papi/subtitles.go
@@ -56,6 +56,40 @@ func (s *Subtitle) downloadURL() (string, error) {
 	return uri + "/download", nil
 }
 
+// SubtitleEntry represents a subtitle entry returned by the available subtitles listing.
+type SubtitleEntry struct {
+	Index int `json:"index"`
+	*polochon.SubtitleEntry
+}
+
+// ListAvailableSubtitles returns the list of available subtitles for a video.
+func (c *Client) ListAvailableSubtitles(video polochon.Video, lang polochon.Language) ([]*SubtitleEntry, error) {
+	s := &Subtitle{Subtitle: &polochon.Subtitle{Video: video, Lang: lang}}
+	uri, err := s.uri()
+	if err != nil {
+		return nil, err
+	}
+
+	var entries []*SubtitleEntry
+	err = c.get(fmt.Sprintf("%s/%s/available", c.endpoint, uri), &entries)
+	return entries, err
+}
+
+// DownloadSubtitleByIndex downloads a specific subtitle by its index from the available listing.
+func (c *Client) DownloadSubtitleByIndex(video polochon.Video, lang polochon.Language, index int) (*Subtitle, error) {
+	s := &Subtitle{Subtitle: &polochon.Subtitle{Video: video, Lang: lang}}
+	uri, err := s.uri()
+	if err != nil {
+		return nil, err
+	}
+
+	result := &Subtitle{Subtitle: &polochon.Subtitle{}}
+	err = c.post(fmt.Sprintf("%s/%s/available", c.endpoint, uri), struct {
+		Index int `json:"index"`
+	}{Index: index}, &result)
+	return result, err
+}
+
 // UpdateSubtitle updates the subtitles of a ressource
 func (c *Client) UpdateSubtitle(video polochon.Video, lang polochon.Language) (*Subtitle, error) {
 	s := &Subtitle{Subtitle: &polochon.Subtitle{
