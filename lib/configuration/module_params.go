bd5691055f68a1561e5972c72d06f97adb3f0692
Grégoire Delattre <gregoire.delattre@gmail.com>
2026-04-03 11:50:24 +0200

    Rework the module loader to use generics

--- lib/configuration/module_params.go
+++ lib/configuration/module_params.go
@@ -2,18 +2,11 @@ package configuration
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"log/slog"
 
 	polochon "github.com/odwrtw/polochon/lib"
 	"gopkg.in/yaml.v2"
-)
-
-// Custom errors
-var (
-	ErrMissingDetailerNames  = errors.New("configuration: missing detailer names")
-	ErrMissingTorrenterNames = errors.New("configuration: missing torrenter names")
 )
 
 // ModulesParams holds the module params in raw yaml
@@ -70,155 +63,29 @@ func (mp ModulesParams) getModule(t polochon.ModuleType, name string, log *slog.
 	return module, module.Init(mp.params[name], log)
 }
 
-func (mp ModulesParams) getModules(t polochon.ModuleType, log *slog.Logger, names ...string) ([]polochon.Module, error) {
-	modules := []polochon.Module{}
-
-	for _, name := range names {
-		module, err := mp.getModule(t, name, log)
+func getModulesAs[T any](mp *ModulesParams, t polochon.ModuleType, log *slog.Logger, names []string) ([]T, error) {
+	if len(names) == 0 {
+		return nil, nil
+	}
+	res := make([]T, len(names))
+	for i, name := range names {
+		m, err := mp.getModule(t, name, log)
 		if err != nil {
 			return nil, err
 		}
-		modules = append(modules, module)
-	}
-
-	return modules, nil
-}
-
-func (mp ModulesParams) getDetailers(names []string, log *slog.Logger) ([]polochon.Detailer, error) {
-	if len(names) == 0 {
-		return nil, ErrMissingDetailerNames
-	}
-
-	modules, err := mp.getModules(polochon.TypeDetailer, log, names...)
-	if err != nil {
-		return nil, err
-	}
-
-	res := []polochon.Detailer{}
-	for _, m := range modules {
-		res = append(res, m.(polochon.Detailer))
+		res[i] = m.(T)
 	}
 	return res, nil
 }
 
-func (mp ModulesParams) getSubtitlers(names []string, log *slog.Logger) ([]polochon.Subtitler, error) {
-	modules, err := mp.getModules(polochon.TypeSubtitler, log, names...)
+func getModuleAs[T any](mp *ModulesParams, t polochon.ModuleType, log *slog.Logger, name string) (T, error) {
+	var zero T
+	if name == "" {
+		return zero, nil
+	}
+	m, err := mp.getModule(t, name, log)
 	if err != nil {
-		return nil, err
+		return zero, err
 	}
-
-	res := []polochon.Subtitler{}
-	for _, m := range modules {
-		res = append(res, m.(polochon.Subtitler))
-	}
-	return res, nil
-}
-
-func (mp ModulesParams) getExplorers(names []string, log *slog.Logger) ([]polochon.Explorer, error) {
-	modules, err := mp.getModules(polochon.TypeExplorer, log, names...)
-	if err != nil {
-		return nil, err
-	}
-
-	res := []polochon.Explorer{}
-	for _, m := range modules {
-		res = append(res, m.(polochon.Explorer))
-	}
-	return res, nil
-}
-
-func (mp ModulesParams) getSearchers(names []string, log *slog.Logger) ([]polochon.Searcher, error) {
-	modules, err := mp.getModules(polochon.TypeSearcher, log, names...)
-	if err != nil {
-		return nil, err
-	}
-
-	res := []polochon.Searcher{}
-	for _, m := range modules {
-		res = append(res, m.(polochon.Searcher))
-	}
-	return res, nil
-}
-
-func (mp ModulesParams) getWishlisters(names []string, log *slog.Logger) ([]polochon.Wishlister, error) {
-	modules, err := mp.getModules(polochon.TypeWishlister, log, names...)
-	if err != nil {
-		return nil, err
-	}
-
-	res := []polochon.Wishlister{}
-	for _, m := range modules {
-		res = append(res, m.(polochon.Wishlister))
-	}
-	return res, nil
-}
-
-func (mp ModulesParams) getNotifiers(names []string, log *slog.Logger) ([]polochon.Notifier, error) {
-	modules, err := mp.getModules(polochon.TypeNotifier, log, names...)
-	if err != nil {
-		return nil, err
-	}
-
-	res := []polochon.Notifier{}
-	for _, m := range modules {
-		res = append(res, m.(polochon.Notifier))
-	}
-	return res, nil
-}
-
-func (mp ModulesParams) getTorrenters(names []string, log *slog.Logger) ([]polochon.Torrenter, error) {
-	if len(names) == 0 {
-		return nil, ErrMissingTorrenterNames
-	}
-
-	modules, err := mp.getModules(polochon.TypeTorrenter, log, names...)
-	if err != nil {
-		return nil, err
-	}
-
-	torrenters := []polochon.Torrenter{}
-	for _, m := range modules {
-		torrenters = append(torrenters, m.(polochon.Torrenter))
-	}
-	return torrenters, nil
-}
-
-func (mp ModulesParams) getFsNotifier(name string, log *slog.Logger) (polochon.FsNotifier, error) {
-	module, err := mp.getModule(polochon.TypeFsNotifier, name, log)
-	if err != nil {
-		return nil, err
-	}
-
-	return module.(polochon.FsNotifier), nil
-}
-
-func (mp ModulesParams) getGuessers(names []string, log *slog.Logger) ([]polochon.Guesser, error) {
-	modules, err := mp.getModules(polochon.TypeGuesser, log, names...)
-	if err != nil {
-		return nil, err
-	}
-
-	res := []polochon.Guesser{}
-	for _, m := range modules {
-		res = append(res, m.(polochon.Guesser))
-	}
-	return res, nil
-}
-
-func (mp ModulesParams) getDownloader(name string, log *slog.Logger) (polochon.Downloader, error) {
-	module, err := mp.getModule(polochon.TypeDownloader, name, log)
-	if err != nil {
-		return nil, err
-	}
-
-	return module.(polochon.Downloader), nil
-}
-
-func (mp ModulesParams) getCalendar(name string, log *slog.Logger) (polochon.Calendar, error) {
-	module, err := mp.getModule(polochon.TypeCalendar, name, log)
-	if err != nil {
-		return nil, err
-	}
-
-	return module.(polochon.Calendar), nil
+	return m.(T), nil
 }
